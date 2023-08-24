7552a688017ff54c602e844391a866130a4c6cfd
Adam-Smart <adams8632@hotmail.co.uk>
2023-08-24 16:24:20 +0100

    fixed seeding error

--- db/seeds.sql
+++ db/seeds.sql
@@ -10,7 +10,7 @@ VALUES (1, "Stock Replen", 18000, 1),
        (3, "Till Operator", 13000, 4),
        (4, "Customer Service", 11000, 4),
        (5, "Baker", 11000, 3),
-       (6, "Runner", 12500, 4),
+       (6, "Runner", 12500, 4);
        
 INSERT INTO employee (id, firstname, lastname, role_id, manager_id)
 VALUES (1, "Tim", "LeClerc", 1, NULL),
@@ -20,4 +20,4 @@ VALUES (1, "Tim", "LeClerc", 1, NULL),
        (5, "Charlotte", "Alonso", 3, NULL),
        (6, "Joel", "Bottas", 3, NULL),
        (7, "Trent", "Russel", 4, NULL),
-       (8, "Toby", "Hamilton", 4, NULL),
+       (8, "Toby", "Hamilton", 4, NULL);
